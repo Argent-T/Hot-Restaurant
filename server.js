16fef1ae75838578a4c8aca5dfd8ae7dca9d44f6
Johan Bueno <johan.s.bueno@gmail.com>
2020-01-04 11:09:22 -0500

    server

--- server.js
+++ server.js
@@ -24,3 +24,49 @@ var waitlist = [{
     email: "test1@email.com",
     id: "11111"
 }];
+
+// routing
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+  app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+  });
+  app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
+
+
+app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newTables = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newTables.routeName = newTables.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newTables);
+
+  if (tables.length<6){
+    tables.push(newTables);
+  } else {
+      waitlist.push(newTables);
+      alert ("you been added to the waiting list")
+  }
+    
+  
+    res.json(newTables);
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
