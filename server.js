72487df7eefccc1bdb8059bc70b6fa01968681d2
Kacie Hatley <kaciehatley@gmail.com>
2020-01-04 13:15:01 -0500

    emptied arrays

--- server.js
+++ server.js
@@ -13,19 +13,9 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var tables = [{
-  name: "tom",
-  phoneNumber: "000-000-0000",
-  email: "test@email.com",
-  id: "12345"
-}];
+var tables = [];
 
-var waitlist = [{
-  name: "bob",
-  phoneNumber: "111-111-1111",
-  email: "test1@email.com",
-  id: "11111"
-}];
+var waitlist = [];
 
 // routing
 
